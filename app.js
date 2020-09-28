266b890f41384231943cfe6f31ff41c197f4f526
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-09-28 16:56:23 +0200

    add let

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-module.exports = (str) => {
-  return `🦄${str}🦄`;
-};
+const axios = require('axios')
+const URL = 'https://pokeapi.co/api/v2/pokemon'
+
+// let checkNameOfPokemon = async function(){
+//   try {
+//     const response = await axios.get(URL);
+//     console.log(response.data.results);
+//     return response;
+//   } 
+//   catch (err) {
+//     throw "Unable to fetch current Pokemon";
+//   }
+// }
+
+// module.exports = (str) => {
+//   return `🦄${str}🦄`;
+// };
+
+let nom = 'Alex'
+
+module.exports = {
+  // checkNameOfPokemon: checkNameOfPokemon, 
+  nom:nom
+}
